ce0dee1a534ab91dd549e649fa4e7b16dc220aeb
mend-dev <mendbookdev@gmail.com>
2024-10-30 18:09:31 -0500

    Updates

---
+++ _app/immutable/chunks/paths.BXlYc4cy.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_u6cvby)==null?void 0:s.base)??"/bcs-database";var a;const t=((a=globalThis.__sveltekit_u6cvby)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/entry/start.CQMBhf95.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.z_wuCFqV.js";export{t as start};
